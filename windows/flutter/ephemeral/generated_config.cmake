a59e9749a54fefc20c010ce5834f71c8b1ad7ed6
mohdaim996 <60567659+mohdaim996@users.noreply.github.com>
2022-06-03 17:58:36 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter10" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter10"
+  "PROJECT_DIR=C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company"
+  "FLUTTER_ROOT=D:\\flutter10"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company"
+  "FLUTTER_TARGET=C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Asus\\Documents\\Smart Civil Company\\smart_civil_company\\.dart_tool\\package_config.json"
+)
