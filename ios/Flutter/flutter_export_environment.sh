53a404edcf79acfe560ce6b9bc82820015f2113f
mohdaim996 <moh.daim996@gmail.com>
2022-06-03 18:12:12 +0300

    init

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\flutter10"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Asus\Documents\Smart Civil Company\smart_civil_company"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Asus\Documents\Smart Civil Company\smart_civil"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
